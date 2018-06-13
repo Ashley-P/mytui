64cd8aa666ae0e6ba3848d140bd0beaf32cb63de
Ashley-P <ashleyp1244@hotmail.co.uk>
2018-06-13 05:16:27 +0100

    Started work on draw_button Fixed issue in draw_box where it placed the box at the wrong coords Fixed issue in draw_box where if the box was too large, it would still draw it anyway

--- src/draw.c
+++ src/draw.c
@@ -11,8 +11,15 @@ void reset_buf(wchar_t *buf) {
 }
 
 void draw_box(wchar_t *buf, int x, int y, const int width, const int height, const bool fill) {
-    if (x + width > sn_screenwidth || y + width > sn_screenheight)
+    // Shifting the coords by -1, -1 to make sense on the screen
+    x = x - 1;
+    y = y - 1;
+
+    // Checking if the box goes off the screen to prevent weird drawing issues
+    if (x + width > sn_screenwidth || y + width > sn_screenheight) {
         tui_err("draw_box: parameters too large", 0);
+        return;
+    }
 
     // Filled box
     if (fill == true) {
@@ -26,9 +33,9 @@ void draw_box(wchar_t *buf, int x, int y, const int width, const int height, con
     // Non-filled box
     if (fill == false) {
         for(int i = 0; i < height; i++) {
-            *(buf + (x + i) + (y * sn_screenwidth)) = L'#';                 // Top Border
+            *(buf + (x + i) + (y * sn_screenwidth)) = L'#';                     // Top Border
             *(buf + (x + i) + ((y + height - 1) * sn_screenwidth)) = L'#';      // Bottom Border
-            *(buf + x + ((y + i) * sn_screenwidth)) = L'#';                 // Left Border
+            *(buf + x + ((y + i) * sn_screenwidth)) = L'#';                     // Left Border
             *(buf + (x + height - 1) + ((y + i) * sn_screenwidth)) = L'#';      // Right border
         }
     }
@@ -39,4 +46,8 @@ void draw_str(wchar_t *buf, const wchar_t *str, int x, int y) {
     for(int i = 0; i < str_len; i++) {
         *(buf + (x + i) + (y * sn_screenwidth)) = *(str + i);
     }
+}
+
+void draw_button(wchar_t *buf, sButton * button) {
+    draw_box(buf, button->px, button->py, button->width, button->height, 0);
 }
